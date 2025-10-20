6be3c1d788622fc9c2e473be6c1a03e71b2325c6
Henry McConville <henry.mcconville@dojo.tech>
2025-10-20 16:36:59 +0100

    Improve release script

--- release-version.sh
+++ release-version.sh
@@ -7,18 +7,17 @@ git checkout master
 git pull origin master
 
 version=$(cat VERSION)
-echo "version: $version"
-
-# exit if tag already exists.
-if [ $(git tag -l "$version") ]; then
-  echo "tag already exists. Ensure version number has been update in VERSION."
-  exit 1
-fi
+echo "Version: $version"
 
 # check version is in the correct format.
-if ! [[ "$version" =~ ^[0-9.]+$ ]]; then
-  echo "version: "$version" is in the wrong format."
+if ! [[ "$version" =~ ^v[0-9.]+$ ]]; then
+  echo "Version ($version) is in the wrong format."
   exit 1
 fi
 
-docker buildx build --platform linux/amd64,linux/arm64 -t githubexporter/github-exporter:$version --push .
+if docker manifest inspect githubexporter/github-exporter:$version > /dev/null 2>&1; then
+    echo "Image for version ($version) already exists on the registry. Skipping build."
+    exit 1
+fi
+
+docker buildx build --platform linux/amd64,linux/arm64 -t githubexporter/github-exporter:latest -t githubexporter/github-exporter:$version --push .
